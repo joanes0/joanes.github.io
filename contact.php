7233139de2292e2571b50baca89c26e7d2170f90
joanes0 <joanesodanga464@gmail.com>
2019-12-26 11:59:07 +0300

    html and contact files

--- contact.php
+++ contact.php
@@ -4,16 +4,19 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST ['message'];
 
-$email_from = '';
+$email_from = 'joanes0.github.io';
 $email_subject = "New form submission";
-$email_body = "User name:$name.\n".
-                "User Email:$visitor_email.\n".
-                "User Message:$message.\n";
+$email_body = "Name:$name.\n".
+                "Email:$email.\n".
+                "Message:$message.\n";
 
 $to = "joanesodanga464@gmail.com";
 $headers = "From:$email_from\r\n";
 $headers .= "Reply-To:$visitor_email \r\n";
+
 mail($to,$email_subject,$email_body,$headers);
+
+header("location: index.html");
 
 
 
